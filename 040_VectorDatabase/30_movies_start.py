4ecef645f5bbc934d0b2481fba2d4af8af18da3f
dexterchan <dexterchan@gmail.com>
2024-06-11 23:20:05 +0800

    vector db movies

--- 040_VectorDatabase/30_movies_start.py
+++ 040_VectorDatabase/30_movies_start.py
@@ -1,4 +1,4 @@
-#%% packages
+# %% packages
 import re
 import pandas as pd
 import seaborn as sns
@@ -6,14 +6,17 @@ from langchain.text_splitter import SentenceTransformersTokenTextSplitter
 import chromadb
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from pprint import pprint
+
+
 # %% max_length
-def max_word_count(txt_list:list):
+def max_word_count(txt_list: list):
     max_length = 0
     for txt in txt_list:
-        word_count = len(re.findall(r'\w+', txt))
+        word_count = len(re.findall(r"\w+", txt))
         if word_count > max_length:
             max_length = word_count
     return f"Max Word Count: {max_length} words"
+
 
 # %% Sentence splitter
 # chroma default sentence model "all-MiniLM-L6-v2"
@@ -23,25 +26,28 @@ model_max_chunk_length = 256
 token_splitter = SentenceTransformersTokenTextSplitter(
     tokens_per_chunk=model_max_chunk_length,
     model_name="all-MiniLM-L6-v2",
-    chunk_overlap=0
+    chunk_overlap=0,
 )
 
 # %% Data Import
-
-#%% filter movies for missing title or overwiew
-
+data_file = "../data/movies.csv"
+df = pd.read_csv(data_file, parse_dates=["release_date"])
+df.head()
+# %% filter movies for missing title or overwiew
+df_movies_filtered = df.dropna(subset=["title", "overview"])
 # filter for unique ids
 
-
-#%%
-
+df_movies_filtered.drop_duplicates(subset=["id"], inplace=True)
+# %%
+# Check the shape here
+df_movies_filtered.shape
 
 # %%
 
-#%% Word Distribution
+# %% Word Distribution
 
 
-#%% visualize token distribution
+# %% visualize token distribution
 
 # %%
 
@@ -49,22 +55,22 @@ token_splitter = SentenceTransformersTokenTextSplitter(
 # chroma_db = chromadb.Client()  # on the fly
 # persistent
 
-#%% List Collections
+# %% List Collections
 
-#%% Get / Create Collection
+# %% Get / Create Collection
 
 # %% add all tokens to collection
 
-#%% Add movies to vector DB
+# %% Add movies to vector DB
 
 
-#%% count of documents in collection
+# %% count of documents in collection
 
 
 # %% Function to get title
 
 
-#%% Test the function
+# %% Test the function
 
 
 # %%
