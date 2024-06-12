1c556329213c0390fe642285a4887b15f53ac317
dexterchan <dexterchan@gmail.com>
2024-06-12 22:47:05 +0800

    vector db

--- 040_VectorDatabase/30_movies_start.py
+++ 040_VectorDatabase/30_movies_start.py
@@ -43,36 +43,61 @@ df_movies_filtered.drop_duplicates(subset=["id"], inplace=True)
 df_movies_filtered.shape
 
 # %%
-max_word_count(df_movies_filtered['overview'])
+max_word_count(df_movies_filtered["overview"])
 # %% Word Distribution
 description_len = []
 for txt in df_movies_filtered.loc[:, "overview"]:
-    description_len.append(len(re.findall(r'\w+', txt)))
+    description_len.append(len(re.findall(r"\w+", txt)))
 
 # %% visualize token distribution
-
-# %%
+sns.histplot(description_len)
+# %% embedding function
+embedding_fn = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
 # %%
 # chroma_db = chromadb.Client()  # on the fly
 # persistent
+chroma_db = chromadb.PersistentClient("database.db")
 
 # %% List Collections
+chroma_db.list_collections()
 
 # %% Get / Create Collection
+chroma_collection = chroma_db.get_or_create_collection(
+    name="moveies", embedding_function=embedding_fn
+)
 
 # %% add all tokens to collection
-
+ids = df_movies_filtered["id"].values.astype(str).tolist()
+documents = df_movies_filtered["overview"].values.tolist()
+titles = df_movies_filtered["title"].values.tolist()
+metadatas = [{"title": t} for t in titles]
 # %% Add movies to vector DB
+# add documents in batches
+BATCH_SIZE: int = 5000
+for i in range(0, len(ids), BATCH_SIZE):
+    begin, end = i, i + BATCH_SIZE
+    print(f"Processing batch {begin}:{end}")
+    chroma_collection.add(
+        ids=ids[begin:end],
+        metadatas=metadatas[begin:end],
+        documents=documents[begin:end],
+    )
 
 
 # %% count of documents in collection
+len(chroma_collection.get()["ids"])
 
 
 # %% Function to get title
+def get_title_by_description(query_text: str) -> str:
+    n_best = 3
+    res = chroma_collection.query(query_texts=[query_text], n_results=n_best)
+
+    return [res["metadatas"][0][i]["title"] for i in range(n_best)]
 
 
 # %% Test the function
-
+get_title_by_description(query_text="super big lizard")
 
 # %%
