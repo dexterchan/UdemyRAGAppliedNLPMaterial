ac1cd0e8d304f9fbe984e98d61bdfa9566080a2c
dexterchan <dexterchan@gmail.com>
2024-07-01 22:10:02 +0800

    self critique

--- 095_AdvancedPromptEngineering/40_SelfCritique_end.py
+++ 095_AdvancedPromptEngineering/40_SelfCritique_end.py
@@ -1,12 +1,26 @@
-#%% packages
+# %% packages
 # related to: https://medium.com/@cobusgreyling/self-critique-llm-chain-using-langchain-smartllmchain-d67c42a4fa83
 # run pip install langchain_experimental
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain_experimental.smart_llm import SmartLLMChain
-# %% import SmartLLMChain
+
 # %%
 hard_question = "If it takes 5 workers 8 days to build a house, how long would it take 10 workers to build the same house?"
+model_name = "gpt-3.5-turbo"
+# %% baseline
+import openai
+from openai import OpenAI
+import os
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+openai_client = OpenAI()
+messages = [{"role": "user", "content": hard_question}]
+response = openai_client.chat.completions.create(model=model_name, messages=messages)
+content = response.choices[0].message.content
+print(f"baseline zero-shot: {content}")
+
+# %% import SmartLLMChain
 
 prompt = PromptTemplate.from_template(hard_question)
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
