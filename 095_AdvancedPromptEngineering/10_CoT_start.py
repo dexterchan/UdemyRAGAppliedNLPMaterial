208ca5430365a8f569bdd340e1d4942a94cc1538
dexterchan <dexterchan@gmail.com>
2024-07-01 10:52:29 +0800

    adv feedback

--- 095_AdvancedPromptEngineering/10_CoT_start.py
+++ 095_AdvancedPromptEngineering/10_CoT_start.py
@@ -1,20 +1,37 @@
-#%% packages
+# %% packages
 import openai
 from openai import OpenAI
 import pandas as pd
 import os
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 from pprint import pprint
+
 # %%
 openai_client = OpenAI()
 
 # %% Function Definition
-model_name = "gpt-3.5-turbo"
+model_name = "gpt-4o"
 
-def chain_of_thought(user_query:str):
-    pass
 
-#%% Test
+def chain_of_thought(user_query: str):
+    system_role_definition: str = (
+        "You are a helpful assitant and answer precise and concise."
+    )
+    user_query_complete = f"{user_query}, think step by step"
+
+    messages = [
+        {"role": "system", "content": system_role_definition},
+        {"role": "user", "content": user_query_complete},
+    ]
+    response = openai_client.chat.completions.create(
+        model=model_name, messages=messages, max_tokens=4096
+    )
+    content = response.choices[0].message.content
+    return content
+
+
+# %% Test
 user_prompt = "The goal of the Game of 24 is to use the four arithmetic operations (addition, subtraction, multiplication, and division) to combine four numbers and get a result of 24. The numbers are 3, 4, 6, and 8."
 res = chain_of_thought(user_query=user_prompt)
 pprint(res)
