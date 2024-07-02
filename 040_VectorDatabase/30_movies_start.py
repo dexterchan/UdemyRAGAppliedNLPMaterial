90394f0984d12076982d44cc87887b771d893222
dexterchan <dexterchan@gmail.com>
2024-07-02 22:19:11 +0800

    rag demo completed

--- 040_VectorDatabase/30_movies_start.py
+++ 040_VectorDatabase/30_movies_start.py
@@ -44,7 +44,6 @@ df_movies_filtered.shape
 
 # %%
 max_word_count(df_movies_filtered["overview"])
-max_word_count(df_movies_filtered["overview"])
 # %% Word Distribution
 description_len = []
 for txt in df_movies_filtered.loc[:, "overview"]:
@@ -55,32 +54,22 @@ sns.histplot(description_len)
 # %% embedding function
 embedding_fn = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 sns.histplot(description_len)
-# %% embedding function
-embedding_fn = SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+
 
 # %%
 # chroma_db = chromadb.Client()  # on the fly
 # persistent
 chroma_db = chromadb.PersistentClient("database.db")
-chroma_db = chromadb.PersistentClient("database.db")
 
 # %% List Collections
-chroma_db.list_collections()
 chroma_db.list_collections()
 
 # %% Get / Create Collection
 chroma_collection = chroma_db.get_or_create_collection(
-    name="moveies", embedding_function=embedding_fn
-)
-chroma_collection = chroma_db.get_or_create_collection(
-    name="moveies", embedding_function=embedding_fn
+    name="movies", embedding_function=embedding_fn
 )
 
 # %% add all tokens to collection
-ids = df_movies_filtered["id"].values.astype(str).tolist()
-documents = df_movies_filtered["overview"].values.tolist()
-titles = df_movies_filtered["title"].values.tolist()
-metadatas = [{"title": t} for t in titles]
 ids = df_movies_filtered["id"].values.astype(str).tolist()
 documents = df_movies_filtered["overview"].values.tolist()
 titles = df_movies_filtered["title"].values.tolist()
@@ -96,17 +85,6 @@ for i in range(0, len(ids), BATCH_SIZE):
         metadatas=metadatas[begin:end],
         documents=documents[begin:end],
     )
-# add documents in batches
-BATCH_SIZE: int = 5000
-for i in range(0, len(ids), BATCH_SIZE):
-    begin, end = i, i + BATCH_SIZE
-    print(f"Processing batch {begin}:{end}")
-    chroma_collection.add(
-        ids=ids[begin:end],
-        metadatas=metadatas[begin:end],
-        documents=documents[begin:end],
-    )
-
 
 # %% count of documents in collection
 len(chroma_collection.get()["ids"])
@@ -121,7 +99,6 @@ def get_title_by_description(query_text: str) -> str:
 
 
 # %% Test the function
-get_title_by_description(query_text="super big lizard")
 get_title_by_description(query_text="super big lizard")
 
 # %%
--- 050_RAG/20_rag_start.py
+++ 050_RAG/20_rag_start.py
@@ -1,47 +1,104 @@
-#%% packages
+# %% packages
 import os
 from pprint import pprint
 import chromadb
 import openai
 from openai import OpenAI
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 # chroma_db = chromadb.Client()  # on the fly
-chroma_db = chromadb.PersistentClient(path="db")
+chroma_db = chromadb.PersistentClient(path="./database.db")
 
-#%% List Collections
+# %% List Collections
+chroma_db.list_collections()
 
-#%% Get / Create Collection
+# %% Get / Create Collection
+movies_collection = chroma_db.get_or_create_collection(name="movies")
 
 
-
-#%% count of documents in collection
+# %% count of documents in collection
+len(movies_collection.get()["ids"])
 
 
 # %% Query Function
+def get_query_results(query_text: str, n_results: int = 5):
+    res = movies_collection.query(query_texts=[query_text], n_results=n_results)
+    docs = res["documents"][0]
 
+    titles = [item["title"] for item in res["metadatas"][0]]
+    res_string = ";".join(
+        [f"{title}:({description})" for title, description in zip(titles, docs)]
+    )
 
+    return res_string
 
 
 # %% RAG setup
+system_role_definition: str = """
+You are an expert in moveis.
+Users ask you question about movies.
+You will get a user question and relevant information.
+Relevant information is structured like:
+movie title:movie plot; .... 
+with ";" separating each movie information.
+
+Please answer the question ONLY using the information provided.
+"""
+
+user_query = "What are the names of the movies and their plot where {relevant_info} ?"
+
+
+def generate_llm_prompt(query_text: str, n_results: int = 5) -> list[dict]:
+    relevant_info = get_query_results(query_text=query_text, n_results=n_results)
+
+    messages = [
+        {"role": "system", "content": system_role_definition},
+        {"role": "user", "content": user_query.format(relevant_info=relevant_info)},
+    ]
+    return messages
 
 
 # %% Pass Vector DB response to RAG
+openai_client = OpenAI()
+model = "gpt-3.5-turbo"
+query = "huge lizard"
+openai_response = openai_client.chat.completions.create(
+    model=model,
+    messages=generate_llm_prompt(query),
+    temperature=0.2,
+)
 
-    
+
 # %% Response from RAG
+openai_response.choices[0].message.content
 
 # %%
 
-# %% bundle everything in a function
 
-    
+# %% bundle everything in a function
+def rag(query_text: str, n_results: int = 5) -> list[str]:
+    messages: list[dict] = generate_llm_prompt(
+        query_text=query_text, n_results=n_results
+    )
+
+    openai_response = openai_client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0.2,
+    )
+
+    return openai_response.choices[0].message.content.split("\n")
+
+
 # %% Response from Vector DB
 print("Response from Vector DB")
 print("-------------------------------------------------")
-query = "a cop is chasing a criminal"
+query = "hero save the earth"
+res = rag(query_text=query, n_results=3)
 
-#%%
+pprint(res)
+# %%
+
+# %%
 print("Response from RAG")
 print("-------------------------------------------------")
-
-
