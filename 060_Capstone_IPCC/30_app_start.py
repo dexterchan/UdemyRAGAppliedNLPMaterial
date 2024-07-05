760b0ebf4229cead6902b6fbe67983b1b16365ba
dexterchan <dexterchan@gmail.com>
2024-07-05 21:46:57 +0800

    chatbot

--- 060_Capstone_IPCC/30_app_start.py
+++ 060_Capstone_IPCC/30_app_start.py
@@ -1,27 +1,36 @@
-#%% packages
+# %% packages
 import os
 import streamlit as st
 import chromadb
 import openai
 from openai import OpenAI
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-#%% data prep
+# %% data prep
 chroma_client = chromadb.PersistentClient(path="db")
 chroma_collection = chroma_client.get_or_create_collection("ipcc")
 
-#%%
+
+# %%
 def rag(query, n_results=5):
     res = chroma_collection.query(query_texts=[query], n_results=n_results)
     docs = res["documents"][0]
-    joined_information = ';'.join([f'{doc}' for doc in docs])
+    joined_information = ";".join([f"{doc}" for doc in docs])
     messages = [
         {
             "role": "system",
-            "content": "You are a helpful expert on climate change. Your users are asking questions about information contained in attached information."
-            "You will be shown the user's question, and the relevant information. Answer the user's question using only this information."
+            "content": """
+                        You are a helpful expert on climate change.
+                        Your users are asking questions about information contained in attached information.
+                        Answer the user's question using only this provided information.
+                        Please answer within 200 words.            
+                        """,
         },
-        {"role": "user", "content": f"Question: {query}. \n Information: {joined_information}"}
+        {
+            "role": "user",
+            "content": f"Question: {query}. \n Information: {joined_information}",
+        },
     ]
     openai_client = OpenAI()
     model = "gpt-3.5-turbo"
@@ -32,22 +41,25 @@ def rag(query, n_results=5):
     content = response.choices[0].message.content
     return content, docs
 
-#%%
+
+# %%
 st.header("Climate Change Chatbot")
 
 # text input field
-user_query = st.text_input(label="", help="Ask a question about climate change", placeholder="What do you want to know about climate change?")
+user_query = st.text_input(
+    label="",
+    help="Ask a question about climate change",
+    placeholder="What do you want to know about climate change?",
+)
 
+n_results: int = 5
 
-rag_response, raw_docs =  rag(user_query)
+rag_response, raw_docs = rag(query=user_query, n_results=n_results)
 st.header("Raw Information")
-raw_docs = ["", "", "", "", ""] if len(user_query)==0 else raw_docs
-st.text(f"Raw Response 0: {raw_docs[0]}")
-st.text(f"Raw Response 1: {raw_docs[1]}")
-st.text(f"Raw Response 2: {raw_docs[2]}")
-st.text(f"Raw Response 3: {raw_docs[3]}")
-st.text(f"Raw Response 4: {raw_docs[4]}")
+raw_docs = ["", "", "", "", ""] if len(user_query) == 0 else raw_docs
+for i, doc in enumerate(raw_docs):
+    st.text(f"Raw Response {i}: {doc}")
 
 st.header("RAG Response")
-rag_response = "" if len(user_query)==0 else rag_response
+rag_response = "" if len(user_query) == 0 else rag_response
 st.write(rag_response)
