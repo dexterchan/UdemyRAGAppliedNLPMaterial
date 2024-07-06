83598291e85e5643086f0cd3f8c8b8c03b57f288
dexterchan <dexterchan@gmail.com>
2024-07-06 23:05:06 +0800

    llama3 integration

---
+++ 080_OpenSourceLLM/40_llama_start.py
@@ -0,0 +1,53 @@
+# LLM runner will be llama 3 supported by https://hub.docker.com/r/ollama/ollama
+# trigger Ollama : https://ollama.com/library/llama3
+# %% packages
+import os
+import chromadb
+
+# %% data prep
+chroma_client = chromadb.PersistentClient(path="db")
+chroma_collection = chroma_client.get_or_create_collection("ipcc")
+
+
+# %%
+import requests
+import json
+import time
+
+
+def rag(query, n_results=5):
+    time_start = time.time()
+    res = chroma_collection.query(query_texts=[query], n_results=n_results)
+    docs = res["documents"][0]
+    joined_information = ";".join([f"{doc}" for doc in docs])
+
+    system_prompt: str = """
+                        You are a helpful expert on climate change.
+                        Your users are asking questions about information contained in attached information.
+                        Answer the user's question using only this provided information.
+                        Please answer within 200 words.            
+                        """
+    user_prompt: str = f"Question: {query}. \n Information: {joined_information}"
+
+    prompt: str = f"[INST]<<SYS>>{system_prompt}<</SYS>>{user_prompt}[/INST]"
+
+    # calling ollama
+    url = "http://localhost:11434/api/generate"
+
+    payload = json.dumps({"model": "llama3", "prompt": prompt, "stream": False})
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    time_end = time.time()
+
+    print(f"elapsed time: {time_end-time_start}")
+    return response
+
+
+# %% calling rag
+response = rag("What is the climate change impact to farming?")
+# %%
+from pprint import pprint
+
+pprint(response.json()["response"])
